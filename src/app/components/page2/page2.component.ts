cfc988a615915d678601da47bc86cceb6860ee5e
Abdjalil Brihoum <ou.brihoum@gmail.com>
2023-03-26 23:47:21 +0100

    refactor page 2

--- src/app/components/page2/page2.component.ts
+++ src/app/components/page2/page2.component.ts
@@ -1,19 +1,30 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SharedService } from 'src/app/shared/services/shared.service';
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
   styleUrls: ['./page2.component.scss'],
 })
-export class Page2Component implements OnInit {
+export class Page2Component {
   @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
   @ViewChild('loadingSvg') loadingSvg!: ElementRef<HTMLImageElement>;
-  constructor(public Shared: SharedService) {}
+  constructor(private Shared: SharedService) {}
+
+  pickedCityTowns = this.Shared.pickedCityTowns;
+  pickedTown = this.Shared.pickedTown;
+
+  changeTown(index: number) {
+    this.Shared.changeTown(index);
+    this.refresh();
+  }
+
+  showModal() {
+    this.Shared.showModal();
+  }
 
   refresh() {
+    this.pickedTown = this.Shared.pickedTown;
     this.video.nativeElement.classList.add('lazyload');
     this.loadingSvg.nativeElement.style.display = 'block';
   }
-
-  ngOnInit(): void {}
 }
